31f6f5f0a08c080908dd26f3c1e1daeaac027f28
Douglas Ferreira <dodofp.df@gmail.com>
2021-05-10 11:13:11 -0300

    Updates

---
+++ precache-manifest.3e37f5f1af05d214d6512af39f82b5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa805d7ae962ac39c15c8a53134d03d",
+    "url": "/expenses-wallet/index.html"
+  },
+  {
+    "revision": "2fec2e382fb9ea199b11",
+    "url": "/expenses-wallet/static/css/main.e02a2048.chunk.css"
+  },
+  {
+    "revision": "ed7b0346fc6c7ae6313b",
+    "url": "/expenses-wallet/static/js/2.2965fa4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expenses-wallet/static/js/2.2965fa4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fec2e382fb9ea199b11",
+    "url": "/expenses-wallet/static/js/main.46aa885d.chunk.js"
+  },
+  {
+    "revision": "ed0f1c020f0eb2136abb",
+    "url": "/expenses-wallet/static/js/runtime-main.83aa63e1.js"
+  }
+]);
